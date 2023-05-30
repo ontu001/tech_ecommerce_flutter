35748c1fbcafa617f92a4b3270415973d1dcae13
ontu001 <mdrohanurrahmanontu@gmail.com>
2023-05-30 10:25:49 -0700

    one grid added

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.only(left: 36.0),
+          padding: EdgeInsets.only(left: 30.0),
           child: NestedScrollView(
             headerSliverBuilder: (context, isScrolled) => [
               SliverList(
@@ -277,7 +277,7 @@ class HomeScreen extends StatelessWidget {
               padding: EdgeInsets.symmetric(vertical: 35),
               child: GridView(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, childAspectRatio: 14 / 18),
+                    crossAxisCount: 2, childAspectRatio: 14 / 17.5),
                 padding: EdgeInsets.only(right: 30.0),
                 scrollDirection: Axis.vertical,
                 children: [
@@ -316,6 +316,210 @@ class HomeScreen extends StatelessWidget {
                           ),
                         ),
                       ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(28.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            // changes the position of the shadow
+                          ),
+                        ],
+                        color: Colors.white),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 30,bottom: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image(image: AssetImage('asset/images/image.png')),
+                          SizedBox(height: 15,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Apple Watch",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                              Text("Series 6 . Red",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w300,color: Colors.grey),),
+                              SizedBox(height: 8,),
+                              Text("\$ 350",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w500,color: Colors.blue),),
+
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(28.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            // changes the position of the shadow
+                          ),
+                        ],
+                        color: Colors.white),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 30,bottom: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image(image: AssetImage('asset/images/image.png')),
+                          SizedBox(height: 15,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Apple Watch",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                              Text("Series 6 . Red",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w300,color: Colors.grey),),
+                              SizedBox(height: 8,),
+                              Text("\$ 350",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w500,color: Colors.blue),),
+
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(28.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            // changes the position of the shadow
+                          ),
+                        ],
+                        color: Colors.white),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 30,bottom: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image(image: AssetImage('asset/images/image.png')),
+                          SizedBox(height: 15,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Apple Watch",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                              Text("Series 6 . Red",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w300,color: Colors.grey),),
+                              SizedBox(height: 8,),
+                              Text("\$ 350",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w500,color: Colors.blue),),
+
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(28.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            // changes the position of the shadow
+                          ),
+                        ],
+                        color: Colors.white),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 30,bottom: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image(image: AssetImage('asset/images/image.png')),
+                          SizedBox(height: 15,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Apple Watch",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                              Text("Series 6 . Red",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w300,color: Colors.grey),),
+                              SizedBox(height: 8,),
+                              Text("\$ 350",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w500,color: Colors.blue),),
+
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(28.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            // changes the position of the shadow
+                          ),
+                        ],
+                        color: Colors.white),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 30,bottom: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image(image: AssetImage('asset/images/image.png')),
+                          SizedBox(height: 15,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Apple Watch",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                              Text("Series 6 . Red",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w300,color: Colors.grey),),
+                              SizedBox(height: 8,),
+                              Text("\$ 350",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w500,color: Colors.blue),),
+
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(28.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            // changes the position of the shadow
+                          ),
+                        ],
+                        color: Colors.white),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 30,bottom: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image(image: AssetImage('asset/images/image.png')),
+                          SizedBox(height: 15,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Apple Watch",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                              Text("Series 6 . Red",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w300,color: Colors.grey),),
+                              SizedBox(height: 8,),
+                              Text("\$ 350",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w500,color: Colors.blue),),
+
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
 
                 ],
               ),
