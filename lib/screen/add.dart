6897b5feab15a8d51a8b28ed261239069a0343d5
ontu001 <mdrohanurrahmanontu@gmail.com>
2023-05-31 08:06:22 -0700

    tabbar feature added

--- lib/screen/add.dart
+++ lib/screen/add.dart
@@ -4,34 +4,8 @@ import 'package:flutter/material.dart';
 class Add extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title:Text('My Ads',style: TextStyle(fontSize: 28,color: Colors.black),),
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.add_shopping_cart),
-                text: 'My Ads',
-                height: 101,
-              ),
-              Tab(
-                icon: Icon(Icons.heart_broken_outlined),
-                text: 'My FAvourite',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-
-          ],
-        ),
-      ),
+    return Scaffold(
+      body: Text("null"),
     );
   }
 
